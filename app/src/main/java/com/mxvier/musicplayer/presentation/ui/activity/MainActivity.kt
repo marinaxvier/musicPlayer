43dcf83763ca2716e8a4d7518b6478df24d8f596
Marina Oliveira <marinaxvier@gmail.com>
2025-02-01 18:29:58 -0300

    creating packages

--- app/src/main/java/com/mxvier/musicplayer/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/mxvier/musicplayer/presentation/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mxvier.musicplayer
+package com.mxvier.musicplayer.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mxvier.musicplayer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
